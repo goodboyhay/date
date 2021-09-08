a5b51e169a1d94bd7160776d0f2ff2c654f95562
goodboyhay <nguyenhuynh.quynhtuyen@gmail.com>
2021-09-09 00:44:41 +0700

    first commit

---
+++ client/src/views/About.js
@@ -0,0 +1,31 @@
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+const About = () => {
+	return (
+		<Row className='mt-5' style={{ marginRight: 0 }}>
+			<Col className='text-center'>
+				<Button
+					variant='primary'
+					href='https://www.facebook.com/nhqt98'
+					size='lg'
+				>
+					My Facebook
+				</Button>
+			</Col>
+			<Col className='text-center'>
+				<Button
+					variant='primary'
+					href='https://www.https://github.com/goodboyhay'
+					size='lg'
+				>
+					GitHub
+				</Button>
+			</Col>
+		</Row>
+		
+	)
+}
+
+export default About
